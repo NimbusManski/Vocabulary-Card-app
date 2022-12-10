7853ac7b1ffeffab11ec0ddcd0dfc0cbbbbc889e
ryan <ryan.d.matuszak@gmail.com>
2022-12-10 01:21:06 -0600

    added search feature

--- Flash Card app/app.js
+++ Flash Card app/app.js
@@ -2,9 +2,11 @@
 const addWordBtn = document.getElementById('add-word-btn');
 const searchBtn = document.getElementById('search-word-go');
 const vocabList = document.getElementById('vocabulary-list');
+const searchInput = document.getElementById("filter-word");
 
 let words = [];
 let id = 0;
+let searchTerm = '';
 
 const renderVocabList = (operation, elementId) => {
   const listRoot = document.getElementById('vocabulary-list');
@@ -39,6 +41,30 @@ const renderVocabList = (operation, elementId) => {
     console.log(words)
   }
 };
+
+const revealAllListItems = () => {
+  const nodeList = [...vocabList.children] // list of li elements in vocab ul
+
+}
+
+const inputHandler = (e) => {
+
+  searchTerm = e.target.value;
+  const nodeList = [...vocabList.children];
+  nodeList.forEach(li => {
+    let listDiv = li.children[0] // the div element inside the li element
+    let vocabWord = listDiv.children[0].innerHTML // button is always third element (index 2) inside of div element. getting id from button element
+    
+    if(!vocabWord.includes(searchTerm)){
+      console.log('doesnt match')
+      li.classList.add('hide');
+    }
+    else{
+      console.log('does match')
+      li.classList.remove('hide');
+    }
+  })
+}
 
 // updates the words array then calls render so that html reflects the state of the array
 const addWordHandler = () => {
@@ -79,6 +105,22 @@ const deleteWordHandler = (e) => {
   renderVocabList('DELETE', elementId);
 }
 
+const hideWords = (wordIds) => {
+  const listRoot = document.getElementById('vocabulary-list');
+  const newVocabWord = document.createElement('li');
+  const nodeList = [...vocabList.children] // list of li elements in vocab ul
+  
+  nodeList.forEach((li, i) => {
+    let listDiv = li.children[0] // the div element inside the li element
+    let listDivBtnId = listDiv.children[2].id // button is always third element (index 2) inside of div element. getting id from button element
+    // console.log(wordIds.includes(0))
+    if(wordIds.includes(parseInt(listDivBtnId, 10))){
+      console.log('here')
+      li.classList.add('hide');
+    }
+  })
+}
+
 const searchWordHandler = (e) => {
   const filterWord = document.getElementById('filter-word').value;
   console.log(filterWord)
@@ -97,20 +139,22 @@ const searchWordHandler = (e) => {
 
   console.log(wordFound)
 
-  wordFound.length ? 
-  alert(`Word: ${wordFound[0].word}\nDefinition: ${wordFound[0].definition}`)
-  :
-  alert(`${filterWord} was not found in your vocabulary list`)
-  
-  // for (let i of words) {
-  //   if (i !== words[words.word]) {
-  //     listItem.classList.add('hide');
-  //   } else {
-  //     listItem.classList.remove('hide');
-  //   }
-  // };
+  let wordIds = []
+  for (let i of words) {
+    if (i.word !== filterWord) {
+      // listItem.classList.add('hide');
+      wordIds.push(i.id)
+    } 
+    // else {
+    //   listItem.classList.remove('hide');
+    // }
+  };
+
+  console.log(wordIds)
+  hideWords(wordIds)
 };
 
 addWordBtn.addEventListener('click', addWordHandler);
 searchBtn.addEventListener('click', searchWordHandler);
 vocabList.addEventListener('click', deleteWordHandler);
+searchInput.addEventListener('input', inputHandler)
